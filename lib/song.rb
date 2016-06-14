85837945a81277070001838d09b8c010f203e31c
Steven McGrath <mcgrath.steven7@gmail.com>
2016-06-14 18:48:57 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,63 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = Song.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(name)
+    song = Song.new
+    song.name = name
+    song
+  end
+  def self.create_by_name(name)
+    song = Song.new
+    song.name = name
+    song.save
+    song
+  end
+
+  def self.find_by_name(name)
+    self.all.find do |song|
+      if song.name == name
+        song
+      end
+    end 
+  
+  end
+  def self.find_or_create_by_name(name)
+    if self.all.include?(name) == true
+      return name
+    else
+      self.create_by_name(name)
+    end
+  end
+  def self.alphabetical
+    @@all.sort_by{|song| song.name[0]}
+
+
+  end
+  def self.new_from_filename(filename)
+    song = Song.new
+    @name = filename.split(".").first.split(" - ").last
+    @artist_name = filename.split(".").first.split(" - ").first
+    song.name = @name
+    song.artist_name = @artist_name
+    song
+  end
+  def self.create_from_filename(filename)
+    song = Song.new
+    @name = filename.split(".").first.split(" - ").last
+    @artist_name = filename.split(".").first.split(" - ").first
+    song.name = @name
+    song.artist_name = @artist_name
+    song.save
+    song
+  end
+  def self.destroy_all
+    @@all = []
+  end
+
 end
